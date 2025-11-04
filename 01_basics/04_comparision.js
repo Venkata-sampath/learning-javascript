f212be2f1ee9c7676a637014c90735d750216721
Venkata Sampath Guthina <136415707+Venkata-sampath@users.noreply.github.com>
2025-11-04 14:03:01 +0000

    comparision operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 >= 2)
+// console.log(1 <= 2);
+// console.log(1 == 2)
+// console.log(1 != 2)
+
+// console.log("2">1);
+
+// console.log(null>0);
+// console.log(null==0)
+// console.log(null>=0)
+
+//comparison operators convert null to number = 0 whereas equality operator doesnt
+
+// console.log(undefined>0);
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
+//Prefered to avoid using cross data type comparision as it can cause confusion with output
+
+//=== is used for strict comparision which doesnt convert the datatypes fo the operands
+
+// console.log("2" === 2)//false
+// console.log("2" == 2)//true
+
+
+
