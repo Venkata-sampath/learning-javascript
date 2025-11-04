70a13c7769389703017871ad23040388c2f123f4
Venkata Sampath Guthina <136415707+Venkata-sampath@users.noreply.github.com>
2025-11-04 10:24:13 +0000

    datatypes and code standards

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+2) // we are using nodejs not browser
+
+console.log(3
+    +
+    3)// code readability should be high
+
+console.log("sampath")
+
+let name = "sampath"
+let age = 21
+let isLoggedIn = true
+let state;
+
+//primitive data types
+//number => 2 to ower of 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined
+//symbol => unique
+
+//object => no primitive datatype
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
