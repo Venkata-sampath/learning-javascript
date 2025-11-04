52db6ff7fcc40575601bbfa7e4a1a5d77e7a534c
Venkata Sampath Guthina <136415707+Venkata-sampath@users.noreply.github.com>
2025-11-04 10:46:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+// null => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false
+// "" => false
+// "sampath" => true
+
+let someNumber = true
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// 33 => "33"
+// null => "null"
+// true => "true", false => "false"
+
