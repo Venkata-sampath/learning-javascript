08f916558c6cf34e467a3c63d0446d15095cd03a
Venkata Sampath Guthina <136415707+Venkata-sampath@users.noreply.github.com>
2025-11-05 13:50:55 +0000

    function with array and object

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,34 @@ function loginUserMessage(username = "sam"){ // default value can be determined 
 } 
 
 // console.log(loginUserMessage("sampath"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1){ // ... is rest operator which adds rest of the arguments into the parameters of the function
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 500, 2000))
+
+const user = {
+    username: "sampath",
+    price: 999
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "sampath",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 100, 600]))
