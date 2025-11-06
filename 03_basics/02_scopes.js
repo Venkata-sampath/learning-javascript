8e162f04033b9da82c23b275fd6c3a78fd702cba
Venkata Sampath Guthina <136415707+Venkata-sampath@users.noreply.github.com>
2025-11-06 08:24:07 +0000

    scope and hoisting intro

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,49 @@ let a =300
 if(true){
     let a=10
     const b=20
-    console.log("Inner a: ", a)
+    // console.log("Inner a: ", a)
 }
 
 //console.log(a)
 //console.log(b)
 //console.log(c)
+
+function one(){
+    const username = "sampath"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "sampath"
+    if(username == "sampath"){
+        const website = "youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+// *************************************interesting*************************************
+
+console.log(addOne(4))
+
+function addOne(num){
+    return num+1
+}
+
+console.log(addTwo(4))
+
+const addTwo = function(num){
+    return num+2
+}
+
