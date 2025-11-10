9260b4042cc6d3d74c0df23a06dd46533ae17b41
Venkata Sampath Guthina <136415707+Venkata-sampath@users.noreply.github.com>
2025-11-10 11:24:58 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,14 +3,15 @@
 
 let a =300
 if(true){
-    let a=10
-    const b=20
-    // console.log("Inner a: ", a)
+    let a = 10
+    const b = 20
+    var c = 30
+    // console.log("Inner a: ", a) // output: 10
 }
 
-//console.log(a)
-//console.log(b)
-//console.log(c)
+//console.log(a) // output: 300 
+//console.log(b) // Error b not defined
+//console.log(c) // output: 30
 
 function one(){
     const username = "sampath"
@@ -19,7 +20,7 @@ function one(){
         const website = "youtube"
         console.log(username)
     }
-    // console.log(website)
+    // console.log(website) //  Error website not defined
 
     two()
 }
@@ -32,10 +33,10 @@ if(true){
         const website = "youtube"
         // console.log(username + website)
     }
-    // console.log(website)
+    // console.log(website) // Error website not defined
 }
 
-// console.log(username)
+// console.log(username) // Error username not defined
 
 // *************************************interesting*************************************
 
@@ -45,7 +46,8 @@ function addOne(num){
     return num+1
 }
 
-console.log(addTwo(4))// hoisting concept intro
+// hoisting concept intro
+console.log(addTwo(4)) // Error cannot access addTwo before intialization
 
 const addTwo = function(num){
     return num+2
