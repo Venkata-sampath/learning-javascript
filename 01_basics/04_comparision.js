d4379097e04cd06da36c5f768db97de04cfe2766
Venkata Sampath Guthina <136415707+Venkata-sampath@users.noreply.github.com>
2025-11-10 10:37:49 +0000

    comparsion operators

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,19 +1,21 @@
-// console.log(1 > 2);
-// console.log(1 < 2);
+// comparision operators
+// console.log(1 > 2)
+// console.log(1 < 2)
 // console.log(1 >= 2)
-// console.log(1 <= 2);
+// console.log(1 <= 2)
 // console.log(1 == 2)
 // console.log(1 != 2)
 
-// console.log("2">1);
+// interesting
+// console.log("2">1) // output: true
 
-// console.log(null>0);
+// console.log(null>0)
 // console.log(null==0)
 // console.log(null>=0)
 
 //comparison operators convert null to number = 0 whereas equality operator doesnt
 
-// console.log(undefined>0);
+// console.log(undefined>0)
 // console.log(undefined==0)
 // console.log(undefined>=0)
 
