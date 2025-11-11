5f102d0a0763263ba1461a4cb74705401c79b0a8
Venkata Sampath Guthina <136415707+Venkata-sampath@users.noreply.github.com>
2025-11-11 10:55:26 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,44 @@
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+// for of loop (prints entire key+value)
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    // console.log(num)
+}
+
+const greetings = "Hello sampath"
+for (const greeting of greetings) {
+    // console.log(`Each char is ${greeting}`)
+}
+
+
+// Maps 
+// stores only unique values and in order
+const map = new Map()
+map.set("IN", "India")
+map.set("USA", "United States of America")
+map.set("Fr", "France")
+// console.log(map)
+
+// loop through map 
+for (const key of map) {
+    console.log(key) // check output
+}
+
+// loop through destructured map
+for (const [key, value] of map) {
+    // console.log(key+" :- "+value) // check output
+}
+
+
+// for of doesn't work for looping through objects
+const myObject = {
+    game1: 'NFS',
+    game2: 'spiderman'
+}
+
+// for (const [key, value] of myObject) {
+    // console.log(key+" :- "+value) // Error my object is not iterable
+// }
